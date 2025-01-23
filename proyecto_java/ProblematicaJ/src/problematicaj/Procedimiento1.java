38671c2b205b3031eb93d4df76ab4864f74e1522
jhordycamacas <jhordycamacas9@gmail.com>
2025-01-23 12:48:41 -0500

    cambspr

--- proyecto_java/ProblematicaJ/src/problematicaj/Procedimiento1.java
+++ proyecto_java/ProblematicaJ/src/problematicaj/Procedimiento1.java
@@ -4,10 +4,19 @@
  */
 package problematicaj;
 
+import java.util.Scanner;
+
 /**
  *
  * @author utpl
  */
 public class Procedimiento1 {
+    public static void registrarParticipanteFutbol(){
+           Scanner entrada = new Scanner(System.in);
+           System.out.println("Ingrese nombre del participante");
+           String nombre = entrada.nextLine();
+           
+
+} 
     
 }
